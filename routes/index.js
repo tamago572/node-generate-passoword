6046ca970ab3e1d4cf22510abe0039fa119f4ff1
tamago572 <110117095+tamago572@users.noreply.github.com>
2022-12-07 15:00:34 +0900

    パスワード生成のPOST処理を追加

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Password generator' });
+});
+
+router.post("/generate", (req, res) => {
+  res.send(req.body.msg);
 });
 
 module.exports = router;
