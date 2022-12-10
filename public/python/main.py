02cff7770edbdec93db45a1b252e6664ac0b86f4
tamago572 <110117095+tamago572@users.noreply.github.com>
2022-12-10 23:56:57 +0900

    async/awaitで実行するコードを追加（エラーが発生）

--- public/python/main.py
+++ public/python/main.py
@@ -1,6 +1,10 @@
 # https://github.com/tamago572/pyGeneratePassword から
 import secrets
 import string
+import time
+# import sys
+# data = sys.stdin.readline()
+# num=int(data)
 
 def makePassword(length, isInstructSymbol):
     # 英数字のみ
@@ -20,16 +24,17 @@ isInstructSymbol = "y" # 記号を含めるかどうか (String) y/N
 
 # (nodejsと連携時にする length.isdecimal() で lengthがInt型か判定 ) lengthが1以上 AND lengthが24以下
 if length >= 1 and length <= 24:
-    print('1以上の整数が入力されました\n')
+    # print('1以上の整数が入力されました\n')
     # 記号を含むかを y/nで分岐
     if 'y' in isInstructSymbol:
-        print("記号を含む" + str(length) + "桁のパスワードを生成します")
+        # print("記号を含む" + str(length) + "桁のパスワードを生成します")
         password = makePassword(length, True)
 
     else:
-        print("記号を含まない" + str(length) + "桁のパスワードを生成します")
+        # print("記号を含まない" + str(length) + "桁のパスワードを生成します")
         password = makePassword(length, False)
 
+    time.sleep(2)
     print(password)
 
 else:
--- routes/generate.js
+++ routes/generate.js
@@ -1,8 +1,50 @@
 var express = require('express');
 var router = express.Router();
+// node.jsでPythonを扱うためのライブラリ
+// node.jsのchild_processでの実行に変更する
+var {PythonShell} = require('python-shell');
 
-router.post("/", (req, res) => {
-    res.render('generate', { password: req.body.length });
+
+function pyRun() {
+    let pyshell = new PythonShell('./public/python/main.py');
+    pyshell.on('message', function (data) {
+        // console.log(data);
+        return data;
+    });
+    // end the input stream and allow the process to exit
+    pyshell.end(function (err,code,signal) {
+        if (err) throw err;
+        console.log('The exit code was: ' + code);
+        console.log('The exit signal was: ' + signal);
+        console.log('finished');
+    });
+}
+
+router.post('/', (req, res) => {
+    // returnPassword().then(result => {
+        //     console.log("パスワードを生成しました");
+        //     console.log(result);
+        //     res.send("show");
+        // });
+    async function returnPassword() {
+        try {
+            const password = await pyRun();
+            return password;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    returnPassword().then(result => {
+        console.log(result);
+        // res.send("show");
+        res.render("generate", { password: result })
+    })
+
 });
+
+router.get('/show', (req, res) =>{
+    res.send("This page is show page");
+})
 
 module.exports = router;
